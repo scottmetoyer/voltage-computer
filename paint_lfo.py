ce6c6643872fd704ce9dfc8d77ed1a9f9f9ffef8
Scott Metoyer <scott.metoyer@gmail.com>
2021-03-13 13:14:53 -0800

    More touch test

--- paint_lfo.py
+++ paint_lfo.py
@@ -78,6 +78,9 @@ while running:
       running = False
     elif e.type == MOUSEBUTTONDOWN:
       setButton(e.pos)
+    elif e.type == FINGERDOWN:
+      print("hello")
+      #setButton((e.x, e.y))
 
   screen.fill(black)
 
